bafad1d1d414fc43192b140a18c647b837fbf957
Dan Voell <danvoell@gmail.com>
2012-10-22 10:10:54 -0500

    Finished Sign In

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,7 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_confirmation) }
+   it { should respond_to(:remember_token) }
+   it { should respond_to(:authenticate) }
 end
